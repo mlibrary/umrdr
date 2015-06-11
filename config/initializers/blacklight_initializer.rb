b1ebf00a732511b4f1a9fc3d813ec402e0e282c6
Colin Gross <grosscol@umich.edu>
2015-06-11 16:31:58 -0400

    Track non-senstive config

---
+++ config/initializers/blacklight_initializer.rb
@@ -0,0 +1,7 @@
+# A secret token used to encrypt user_id's in the Bookmarks#export callback URL
+# functionality, for example in Refworks export of Bookmarks. In Rails 4, Blacklight
+# will use the application's secret key base instead.
+#
+
+# Blacklight.secret_key = '93282b6d6bc84d597f282436e2a7aa27e1ff3536c3df9bfcdab08f616618767369b87b3c880e0657038caa9f872feafb55b5a5ed6a7189527a7e4884892edf55'
+
