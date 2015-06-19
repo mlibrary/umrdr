e5461eef98157e03c12d965fd79527aa56e0da67
Colin Gross <grosscol@umich.edu>
2015-06-19 17:48:40 -0400

    Add bash scripts for start/stop app with puma.

---
+++ bin/run-alpha.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+bundle exec puma -d -p 30020 -e alpha --pidfile /hydra-dev/umrdr-alpha.quod/pidfile
---
+++ bin/stop-alpha.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+bundle exec pumactl -P /hydra-dev/umrdr-alpha.quod/pidfile stop
