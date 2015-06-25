5226db43759da8ea270aeb575518a537654ab139
Colin Gross <grosscol@umich.edu>
2015-06-25 11:42:44 -0400

    Explicitly set dev env log_level to debug.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,6 +8,9 @@ end
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  
+  # Logger level debug
+  config.log_level = :debug
 
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
