9627a73a15d6dd2632398cfc258f2cfe067482f7
Colin Gross <grosscol@gmail.com>
2016-02-11 14:36:23 -0500

    Set defaults for work with fewer overrides

--- app/models/generic_work.rb
+++ app/models/generic_work.rb
@@ -7,4 +7,12 @@ class GenericWork < ActiveFedora::Base
   validates :creator, presence: { message: 'Your work must have a creator.' }
   validates :date_created, presence: { message: 'Your work must have a date created.' }
   validates :description, presence: { message: 'Your work must have a description.' }
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    return unless new_record?
+    self.resource_type = ['Dataset']
+    self.visibility = 'open'
+  end
 end
