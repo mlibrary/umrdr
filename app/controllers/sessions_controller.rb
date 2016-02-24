1a59203d2fc294272c513a2d6d089c1d950a1dc0
Aaron Elkiss <aelkiss@umich.edu>
2016-02-24 15:09:31 -0500

    Cosign authentication fixes
     - Don't show user as '(null)' if X-Remote-User header is empty
     - Properly delete Cosign service cookie & destroy user session on logout
     - DRY up Cosign configuration

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,8 @@ class SessionsController < ApplicationController
   def destroy
     # make any local additions here (e.g. expiring local sessions, etc.)
     # adapted from here: http://cosign.git.sourceforge.net/git/gitweb.cgi?p=cosign/cosign;a=blob;f=scripts/logout/logout.php;h=3779248c754001bfa4ea8e1224028be2b978f3ec;hb=HEAD
-    cookies.delete(request.env['COSIGN_SERVICE']) if request.env['COSIGN_SERVICE']
+    sign_out(:user)
+    cookies.delete(Sufia::Engine.config.cosign_service)
     redirect_to Sufia::Engine.config.logout_url
   end
 
