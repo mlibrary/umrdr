93d9ee1f28537b0db8d21ca5bfb81ed07cc1f80e
Roger Espinosa <roger@umich.edu>
2016-02-26 15:48:57 -0500

    customize link_to_profile helper to use sufia.profile_path

--- app/helpers/sufia_helper.rb
+++ app/helpers/sufia_helper.rb
@@ -2,4 +2,19 @@ module SufiaHelper
   include ::BlacklightHelper
   include Sufia::BlacklightOverride
   include Sufia::SufiaHelperBehavior
+
+  def link_to_profile(login)
+    user = ::User.find_by_user_key(login)
+    return login if user.nil?
+
+    text = if user.respond_to? :name
+             user.name
+           else
+             login
+           end
+
+    # sufia_helper_behavior uses Sufia::Engine.routes.url_helpers.profile_path(user) --- WHY?
+    link_to text, sufia.profile_path(user) # 
+  end
+
 end
