74867ddfb51a55bf241b4570c3b686320e4df8c6
Colin Gross <grosscol@gmail.com>
2016-02-27 20:27:07 -0500

    Add shameful hard coded path for user profile.

--- app/helpers/sufia_helper.rb
+++ app/helpers/sufia_helper.rb
@@ -13,8 +13,11 @@ module SufiaHelper
              login
            end
 
-    # sufia_helper_behavior uses Sufia::Engine.routes.url_helpers.profile_path(user) --- WHY?
-    link_to text, sufia.profile_path(user) # 
+    # TODO: Fix the link to the user profiles when the sufia object isn't available.
+    # TODO: Don't hard code relative paths.  Sort out the proper url helper to use here.
+    #   sufia_helper_behavior uses Sufia::Engine.routes.url_helpers.profile_path(user) --- WHY?
+    #   link_to text, sufia.profile_path(user) # works when sufia is available. 
+    link_to text, "/data/users/#{login}"
   end
 
 end
