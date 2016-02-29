e12af39b106c21af6754a77f093cf30038a2a0e0
Colin Gross <grosscol@gmail.com>
2016-02-29 16:07:23 -0500

    Force visibility open for new FileSets.

--- app/models/file_set.rb
+++ app/models/file_set.rb
@@ -3,4 +3,11 @@ class FileSet < ActiveFedora::Base
   include ::CurationConcerns::FileSetBehavior
   include Sufia::FileSetBehavior
   include Umrdr::FileSetBehavior
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    return unless new_record?
+    self.visibility = 'open'
+  end
 end
