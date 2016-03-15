448dd88c49cf7cdf1323e041676fcf52f28af528
Gordon Leacock <gordonl@umich.edu>
2016-03-15 12:23:10 -0400

    add user_spec.rb verify User pw generation and creating w/ just email removed mocks, added real calls to create user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "when a user is created it" do
+    it "can be created with just an email parameter" do
+      u1 = User.create(email: "name@blah.com")
+      expect(u1).to be_an_instance_of(User)
+      expect(u1.email).to eq("name@blah.com")
+    end
+
+    it "doesn't generate a password when called with new" do
+      u2 = User.new
+      expect(u2.password).to be_nil
+    end
+
+    it "doesn't save without a password" do
+      u3 = User.new
+      expect(u3.save).to be_falsey
+    end
+
+    it "can generate a password" do
+      u4 = User.new(email: "name2@blah.com")
+      expect(u4.password).to be_nil
+      u4.generate_password
+      expect(u4.password).to_not be_nil
+      expect(u4.password).to be_kind_of String
+      expect(u4.password_confirmation).to be_kind_of String
+      expect(u4.password).to eq(u4.password_confirmation)
+    end
+  end
 end
