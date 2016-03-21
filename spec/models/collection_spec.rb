e1ac00b2b91db6281612bb0f6e826b94f6746739
Nabeela Jaffer <njaffer@umich.edu>
2016-03-21 11:42:57 -0400

    add collection model spec test

---
+++ spec/models/collection_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+
+describe Collection do
+  let(:user) { stub_model(User, user_key: 'user') }
+  let(:collection) do
+    described_class.new(title: "test collection") do |c|
+      c.apply_depositor_metadata(user)
+    end
+  end
+
+  it "has open visibility" do
+    collection.save
+    expect(collection.read_groups).to eq ['public']
+  end
+
+  it "validates title" do
+    collection.title = nil
+    expect(collection).not_to be_valid
+  end
+end
