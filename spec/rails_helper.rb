f701325005aa1b25f678172bf4f934c3fd58431c
Colin Gross <grosscol@gmail.com>
2016-03-30 15:42:41 -0400

    Add active fedora cleaner to specs. Allows re-running of specs without failure due to prev. Remove generated comments from spec config.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,10 +1,13 @@
-# This file is copied to spec/ when you run 'rails generate rspec:install'
+#require 'database_cleaner'
+require 'active_fedora/cleaner'
+
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 # Prevent database truncation if the environment is production
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
+
 # Add additional requires below this line. Rails is not loaded until this point!
 
 # Requires supporting ruby files with custom matchers and macros, etc, in
@@ -68,4 +71,27 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.before :suite do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.before :each do |example|
+    unless example.metadata[:type] == :view || example.metadata[:no_clean]
+      ActiveFedora::Cleaner.clean!
+    end
+  end
+
+  config.before :each do |example|
+    if example.metadata[:type] == :feature && Capybara.current_driver != :rack_test
+      DatabaseCleaner.strategy = :truncation
+    else
+      DatabaseCleaner.strategy = :transaction
+      DatabaseCleaner.start
+    end
+  end
+
+  config.after do
+    DatabaseCleaner.clean
+  end
 end
