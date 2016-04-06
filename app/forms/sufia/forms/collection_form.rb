483a345d38a93021f8f424d530e0771dea8cad7c
Nabeela Jaffer <njaffer@umich.edu>
2016-04-06 09:58:48 -0400

    Remove contributor from Collection's page

--- app/forms/sufia/forms/collection_form.rb
+++ app/forms/sufia/forms/collection_form.rb
@@ -1,6 +1,6 @@
 module Sufia::Forms
   class CollectionForm < CurationConcerns::Forms::CollectionEditForm
     # Visibility is not settable in Sufia
-    self.terms -= [:visibility, :representative_id, :thumbnail_id, :identifier, :based_near]
+    self.terms -= [:contributor, :visibility, :representative_id, :thumbnail_id, :identifier, :based_near]
   end
 end
