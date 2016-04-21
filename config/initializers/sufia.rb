d525e876b751d8c0cec25152f085d1ef3e0f8afd
Colin Gross <grosscol@gmail.com>
2016-04-21 17:12:54 -0400

    Move resource_types to yaml for CC0.11   Using QA to get resource types.

--- config/initializers/sufia.rb
+++ config/initializers/sufia.rb
@@ -9,29 +9,9 @@ Sufia.config do |config|
   config.max_notifications_for_dashboard = 5
 
   config.resource_types = { "Dataset" => "Dataset" }
+  # TODO move to resource types service and QA after sufia 7.0
+  config.resource_types_to_schema = {"Dataset" => "http://schema.org/Dataset"}
 
-  config.resource_types_to_schema = {
-    "Article" => "http://schema.org/Article",
-    "Audio" => "http://schema.org/AudioObject",
-    "Book" => "http://schema.org/Book",
-    "Capstone Project" => "http://schema.org/CreativeWork",
-    "Conference Proceeding" => "http://schema.org/ScholarlyArticle",
-    "Dataset" => "http://schema.org/Dataset",
-    "Dissertation" => "http://schema.org/ScholarlyArticle",
-    "Image" => "http://schema.org/ImageObject",
-    "Journal" => "http://schema.org/CreativeWork",
-    "Map or Cartographic Material" => "http://schema.org/Map",
-    "Masters Thesis" => "http://schema.org/ScholarlyArticle",
-    "Part of Book" => "http://schema.org/Book",
-    "Poster" => "http://schema.org/CreativeWork",
-    "Presentation" => "http://schema.org/CreativeWork",
-    "Project" => "http://schema.org/CreativeWork",
-    "Report" => "http://schema.org/CreativeWork",
-    "Research Paper" => "http://schema.org/ScholarlyArticle",
-    "Software or Program Code" => "http://schema.org/Code",
-    "Video" => "http://schema.org/VideoObject",
-    "Other" => "http://schema.org/CreativeWork"
-  }
 
   config.permission_levels = {
     "Choose Access" => "none",
