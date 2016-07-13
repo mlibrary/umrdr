de3883ee39cc67479700aabf5c31a89144f13392
Colin Gross <grosscol@gmail.com>
2016-07-13 16:28:23 -0400

    Remove empty work actor.

--- app/actors/curation_concerns/actors/generic_work_actor.rb
+++
@@ -1,8 +0,0 @@
-# Generated via
-#  `rails generate curation_concerns:work GenericWork`
-module CurationConcerns
-  module Actors    
-    class GenericWorkActor < CurationConcerns::Actors::BaseActor
-    end
-  end
-end
