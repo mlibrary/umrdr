a0f54c5feefa41ed6163f7420fa40f4218d26dd6
Nabeela Jaffer <njaffer@umich.edu>
2016-07-25 15:40:08 -0400

    Add help text for citation field

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -110,7 +110,7 @@ class CatalogController < ApplicationController
     config.add_show_field solr_name("resource_type", :stored_searchable), label: "Resource Type"
     config.add_show_field solr_name("format", :stored_searchable), label: "File Format"
     config.add_show_field solr_name("identifier", :stored_searchable), label: "Identifier"
-    config.add_show_field solr_name("isReferencedBy", :stored_searchable), label: "Deep Blue Handle"
+    config.add_show_field solr_name("isReferencedBy", :stored_searchable), label: "Citation"
    
     # "fielded" search configuration. Used by pulldown among other places.
     # For supported keys in hash, see rdoc for Blacklight::SearchFields
@@ -309,7 +309,7 @@ class CatalogController < ApplicationController
     end
 
     config.add_search_field('isReferencedBy') do |field|
-      field.label = "Deep Blue Document Handle"
+      field.label = "Citation"
       # field.solr_parameters = {
       #   :"spellcheck.dictionary" => "description"
       # }
