05eb843f1fbf3bbbad437e922e88ad56d6ee36b0
Colin Gross <grosscol@gmail.com>
2016-09-21 11:26:41 -0400

    Remove erronious flash message after Work creation.   This bug was not an issue ticket.

--- app/controllers/concerns/umrdr/works_controller_behavior.rb
+++ app/controllers/concerns/umrdr/works_controller_behavior.rb
@@ -19,8 +19,6 @@ module Umrdr
     def after_create_response
       respond_to do |wants|
         wants.html do
-          flash[:notice] = "Your files are being processed by #{t('curation_concerns.product_name')} in " \
-            "the background. The metadata and access controls you specified are being applied. " 
           redirect_to [main_app, curation_concern]
         end
         wants.json { render :show, status: :created, location: polymorphic_path([main_app, curation_concern]) }
