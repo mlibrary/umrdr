c0d612a9cb8c44dbd2c8d15605a7a9b95ff2dab4
Blanco <blancoj@158.128-25.43.211.141.in-addr.arpa>
2016-12-01 16:26:35 -0500

    if work is new, only show active licenses. If work is not new, show all licenses. rspec change.
    added curation_concern object in test, since it's now being used.

--- spec/views/batch_edits/edit.html.erb_spec.rb
+++ spec/views/batch_edits/edit.html.erb_spec.rb
@@ -2,12 +2,15 @@ require 'spec_helper'
 
 RSpec.describe 'batch_edits/edit.html.erb' do
   let(:generic_work) { stub_model(GenericWork, id: nil, depositor: 'bob', rights: ['']) }
+  let(:curation_concern) {generic_work}
 
   before do
     allow(controller).to receive(:current_user).and_return(stub_model(User))
     assign :names, ['title 1', 'title 2']
     assign :terms, [:description, :rights]
     assign :generic_work, generic_work
+    allow(view).to receive(:curation_concern).and_return(generic_work)
+
     view.lookup_context.view_paths.push "#{CurationConcerns::Engine.root}/app/views/curation_concerns/base"
     render
   end
