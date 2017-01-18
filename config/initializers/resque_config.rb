4132020f0b898159bc1dd80a75bff863f4286bc9
Bill Dueber <bill@dueber.com>
2017-01-18 16:05:31 -0500

    Read from ENV variable in initializer

--- config/initializers/resque_config.rb
+++ config/initializers/resque_config.rb
@@ -3,5 +3,7 @@ config = YAML.load(ERB.new(IO.read(File.join(Rails.root, 'config', 'redis.yml'))
 Resque.redis = Redis.new(host: config[:host], port: config[:port], thread_safe: true)
 
 Resque.inline = Rails.env.test?
+
 Resque.redis.namespace = ENV['REDIS_NS'] || 'umrdr_dev_of_some_kind'
-Rails.logger.info "Rescue namespace is #{Settings.resque_namespace}"
+Rails.logger.info "Rescue namespace is #{Resque.redis.namespace}"
+Rails.logger.info "Rescue REDIS_NS environment variable is #{ENV['REDIS_NS']}"
