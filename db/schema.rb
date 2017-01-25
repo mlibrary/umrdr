3ab92c10ea3927f08270323f85a1660a7622eab2
Nabeela Jaffer <njaffer@umich.edu>
2017-01-25 14:24:07 -0500

    remove followers table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170125141716) do
+ActiveRecord::Schema.define(version: 20170125153519) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.integer  "user_id",       null: false
