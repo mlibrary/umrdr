f32e4e70a231a0cf8265db1396740dd181eacb51
Nabeela Jaffer <njaffer@umich.edu>
2017-05-09 12:38:21 -0400

    make contact info required

--- app/forms/curation_concerns/generic_work_form.rb
+++ app/forms/curation_concerns/generic_work_form.rb
@@ -5,7 +5,7 @@ module CurationConcerns
     self.model_class = ::GenericWork
     include HydraEditor::Form::Permissions
     self.terms += [:resource_type, :date_coverage]
-    self.required_fields = [ :title, :creator, :methodology, :description, :rights, :subject ]
+    self.required_fields = [ :title, :creator, :methodology, :description, :rights, :subject, :authoremail ]
 
     def rendered_terms
       [ :title, :creator, :authoremail, :methodology, :description, :date_coverage, :rights, :subject, :fundedby, :grantnumber, :keyword, :language, :resource_type, :isReferencedBy, :on_behalf_of, :visibility ]
