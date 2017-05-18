e037f334367d1935b8c1c92c80b078ee14ffa8a6
Roger Espinosa <roger@umich.edu>
2017-05-18 12:36:50 -0400

    renaming to hyrax

--- spec/views/hyrax/collections/_form.html.erb_spec.rb
+++ spec/views/hyrax/collections/_form.html.erb_spec.rb
@@ -1,16 +1,22 @@
-require 'spec_helper'
+require 'rails_helper'
 
-describe 'collections/_form.html.erb' do
+# TODO: is this test a duplicate of the hyrax test?
+
+describe 'hyrax/collections/_form.html.erb' do
   let(:collection) { stub_model(Collection, id: "abc123") }
-  let(:collection_form) { Sufia::Forms::CollectionForm.new(collection) }
+  let(:collection_form) { Hyrax::Forms::CollectionForm.new(collection) }
 
   before do
     controller.request.path_parameters[:id] = 'j12345'
     assign(:form, collection_form)
+    assign(:collection, collection)
+    # Stub route because view specs don't handle engine routes
+    allow(view).to receive(:collections_path).and_return("/collections")
+    allow(view).to receive(:collection_path).and_return("/collections/j12345")
+    render
   end
 
   it "draws the metadata fields for collection" do
-    render
     expect(rendered).to have_selector("input#collection_title")
     expect(rendered).to_not have_selector("div#additional_title.multi_value")
     expect(rendered).to have_selector("input#collection_creator.multi_value")
