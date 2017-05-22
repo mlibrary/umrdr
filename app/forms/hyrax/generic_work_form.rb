5c42147a723727c6e2bf49232e16c7f3b242c5fa
Blanco <blancoj@umich.edu>
2017-05-22 09:32:50 -0400

    make contact a required metadata. fixed test.

--- app/forms/hyrax/generic_work_form.rb
+++ app/forms/hyrax/generic_work_form.rb
@@ -6,7 +6,7 @@ module Hyrax
 
     include HydraEditor::Form::Permissions
     self.terms += [:resource_type, :date_coverage]
-    self.required_fields = [ :title, :creator, :methodology, :description, :rights, :subject ]
+    self.required_fields = [ :title, :creator, :methodology, :description, :rights, :subject, :authoremail ]
 
     def rendered_terms
       [ :title, :creator, :authoremail, :methodology, :description, :date_coverage, :rights, :subject, :fundedby, :grantnumber, :keyword, :language, :resource_type, :isReferencedBy, :on_behalf_of, :visibility ]
