57f681d98e1b46ebc547998559553b06a6a17e29
Blanco <blancoj@umich.edu>
2017-06-21 11:31:27 -0400

    missing filter caused all objects to show up as admin sets.

--- app/models/search_builder.rb
+++ app/models/search_builder.rb
@@ -1,4 +1,8 @@
 class SearchBuilder < Blacklight::SearchBuilder
   include Blacklight::Solr::SearchBuilderBehavior
   include Hydra::AccessControlsEnforcement
+
+  # Without this filter all objects show up as admin sets.
+  include Hyrax::SearchFilters
+
 end
