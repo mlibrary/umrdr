46055cdfdc9b09a54abcbc46168d9bf97867e20b
Jim Eng <jimeng@umich.edu>
2017-06-29 08:40:52 -0400

    UMRDR-626 Send notification when visibility of work is updated from draft to public

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,3 +42,8 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
+
+silence_warnings do
+  require 'pry'
+  IRB = Pry
+end
