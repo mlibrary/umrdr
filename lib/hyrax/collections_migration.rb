c793fbde9d6e1ba9d9087b0d192e44235acd9817
Blanco <blancoj@umich.edu>
2017-08-09 12:37:48 -0400

    in migration, do't validate after saving.

---
+++ lib/hyrax/collections_migration.rb
@@ -0,0 +1,18 @@
+module Hyrax
+  class CollectionsMigration
+    def self.run
+      ::Collection.all.each do |collection|
+        collection.members.each do |member|
+          member.member_of_collections << collection
+          unless member.save(validate: false)
+            raise "Work #{member.id} failed to save after recording its membership in collection #{collection.id}"
+          end
+        end
+        collection.members = []
+        unless collection.save(validate: false)
+          raise "Collection #{collection.id} failed to save after emptying its member list"
+        end
+      end
+    end
+  end
+end
