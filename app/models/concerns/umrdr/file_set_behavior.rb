2e0d4471478a8fb19be0dfe71b7f036c0ade2695
Freiheit <fritx@m-c02v109fj1gp.local>
2017-09-01 12:34:20 -0400

    Added file size limit for create derivatives job.

--- app/models/concerns/umrdr/file_set_behavior.rb
+++ app/models/concerns/umrdr/file_set_behavior.rb
@@ -3,26 +3,5 @@ module Umrdr
     extend ActiveSupport::Concern
 
 
-    # Override the derivatives generation to avoid full text and non-thumbnail
-    # generation.
-    def create_derivatives(filename)
-      case mime_type
-      when *self.class.pdf_mime_types
-        Hydra::Derivatives::PdfDerivatives.create(filename,
-                                                  outputs: [{ label: :thumbnail, format: 'jpg',
-                                                              size: '150x150',
-                                                              }])
-      when *self.class.video_mime_types
-        Hydra::Derivatives::VideoDerivatives.create(filename,
-                                                    outputs: [{ label: :thumbnail, format: 'jpg',
-                                                                size: '150x150',
-                                                                }])
-      when *self.class.image_mime_types
-        Hydra::Derivatives::ImageDerivatives.create(filename,
-                                                    outputs: [{ label: :thumbnail, format: 'jpg',
-                                                                size: '150x150',
-                                                                 }])
-      end
-    end
   end
 end
