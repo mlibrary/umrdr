c5f90d41c35e76344a210eb84f012e586bed490c
Freiheit <fritx@m-c02v109fj1gp.local>
2017-11-16 10:05:25 -0500

    DBD-870-remove-format-restrictions-from-creator-field

--- db/schema.rb
+++ db/schema.rb
@@ -109,9 +109,9 @@ ActiveRecord::Schema.define(version: 20170428174040) do
   end
 
   create_table "follows", force: :cascade do |t|
-    t.string   "followable_type"
+    t.string   "followable_type",                 null: false
     t.integer  "followable_id",                   null: false
-    t.string   "follower_type"
+    t.string   "follower_type",                   null: false
     t.integer  "follower_id",                     null: false
     t.boolean  "blocked",         default: false, null: false
     t.datetime "created_at",                      null: false
