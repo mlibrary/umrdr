9ffe060056a05ff844b40f088a44fc6d862efeb3
Fritz Freiheit <fritx@umich.edu>
2018-02-09 15:37:50 -0500

    DBD-964 - MBox server login
    Partial implementation.

--- app/helpers/box_helper.rb
+++ app/helpers/box_helper.rb
@@ -2,22 +2,66 @@ module BoxHelper
   # https://github.com/cburnette/boxr
   # http://www.rubydoc.info/gems/boxr/Boxr
   require 'boxr'
+  require 'openssl'
 
   class Box
 
+    # {"type"=>"folder", "id"=>"45101723215", "sequence_id"=>"1", "etag"=>"1", "name"=>"ulib-dbd-box"}
+    @@ulib_dbd_box_id = '45101723215'.freeze
+
     attr_accessor :dir_list, :parent_dir
 
-    def initialize( parent_dir: Boxr::ROOT )
+    def initialize( parent_dir: @@ulib_dbd_box_id ) # parent_dir: Boxr::ROOT
       @parent_dir = parent_dir
     end
 
-    def access_token
+    def access_token_developer
       # TODO
       'Token goes here, this will not work work'
+      'NB4wSgTW4HuoQaXnbHfeFHHUeAqED70Y'.freeze
+    end
+
+    def cache_access_and_refresh_tokens( access_token, refresh_token )
+      @access_token = access_token
+      @refresh_token = refresh_token
     end
 
     def client
-      @box_client ||= Boxr::Client.new( access_token )
+      #client_init_developer_token
+      #client_init_v1
+      client_init_json
+      @box_client
+    end
+
+    def client_init_developer_token
+      @access_token = access_token_developer
+      @box_client ||= Boxr::Client.new(access_token_developer )
+    end
+
+    # def client_init_v1
+    #   @access_token = access_token_developer
+    #   token_refresh_callback = lambda { |access, refresh, identifier| cache_access_and_refresh_tokens( access, refresh ) }
+    #   @access_token = 'todo'
+    #   @refresh_token = 'todo'
+    #   @box_client ||= Boxr::Client.new( @access_token,
+    #                        refresh_token: @refresh_token,
+    #                        client_id: 'xa0ut4e8228a8ivf7uz3mjfcm7bnkd41',
+    #                        client_secret: '9N8fN1jPl89N1rOsDaiyhcV0hN5Wxbr1',
+    #                        &token_refresh_callback )
+    # end
+
+    def client_init_json
+      @access_token = access_token_developer
+      token_refresh_callback = lambda { |access, refresh, identifier| cache_access_and_refresh_tokens( access, refresh ) }
+      @box_client ||= Boxr::Client.new( @access_token,
+                                      refresh_token: @refresh_token,
+                                      client_id: json_config_client_id,
+                                      client_secret: json_config_client_secret,
+                                      enterprise_id: json_config_enterprise_id,
+                                      jwt_private_key: json_config_private_key,
+                                      jwt_private_key_password: json_config_passphrase,
+                                      jwt_public_key_id: json_config_public_key_id,
+                                      &token_refresh_callback )
     end
 
     def dir_list
@@ -81,6 +125,45 @@ module BoxHelper
     def parent_dir=( parent_dir )
       @parent_dir = parent_dir
       @dir_list = nil
+    end
+
+    def json_file_path
+      #TODO: move this file to umrder config somewhere
+      '/Users/fritx/Downloads/81663_4yb4bxgd_config.json'
+    end
+
+    def json_config
+      @json_config ||= JSON.parse( File.read( json_file_path ) )
+    end
+
+    def json_config_client_id
+      json_config["boxAppSettings"]["clientID"]
+    end
+
+    def json_config_client_secret
+      json_config["boxAppSettings"]["clientSecret"]
+    end
+
+    def json_config_enterprise_id
+      json_config["enterpriseID"]
+    end
+
+    def json_config_passphrase
+      json_config["boxAppSettings"]["appAuth"]["passphrase"]
+    end
+
+    def json_config_private_key
+      json_config["boxAppSettings"]["appAuth"]["privateKey"]
+    end
+
+    def json_config_public_key_id
+      json_config["boxAppSettings"]["appAuth"]["publicKeyID"]
+    end
+
+    def private_key
+      #rv = OpenSSL::PKey::RSA.new( File.read( ENV['JWT_PRIVATE_KEY_PATH']), ENV['JWT_PRIVATE_KEY_PASSWORD'] )
+      rv = OpenSSL::PKey::RSA.new( json_config_private_key, json_config_passphrase )
+      rv
     end
 
     def upload_link( folder_name )
