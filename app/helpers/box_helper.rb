cd81c977decc4adb5496480fd6abfc8bd0ee629a
Fritz Freiheit <fritx@umich.edu>
2018-03-12 11:32:46 -0400

    DBD-995 - Box not logging in update

--- app/helpers/box_helper.rb
+++ app/helpers/box_helper.rb
@@ -501,7 +501,12 @@ module BoxHelper
       box_id = folder_name_to_box_id( folder_name )
       rv = "https://umich.app.box.com/folder/#{@ulib_dbd_box_id}"
       if !failed_box_login && !box_id.nil?
-        box_link = client.create_shared_link_for_folder( box_id )
+        box_link = client.create_shared_link_for_folder( box_id, can_preview: true, access: 'open' )
+        if !box_link.nil && "open" != box_link.shared_link.permissions.can_preview
+          verbose_log_status( "upload_link", " old link, deleting and recreating" ) if @box_verbose
+          client.disable_shared_link_for_folder( box_id )
+          box_link = client.create_shared_link_for_folder( box_id, can_preview: true, access: 'open' )
+        end
         rv = box_link.shared_link.url
       end
       verbose_log_status( "upload_link", " returning #{rv}" ) if @box_verbose
